152e14f73b870ad8d5926b64027fd59a31c0529b
Nadia Iftekhar <nadiajiftekhar@gmail.com>
2026-02-23 19:45:52 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Sidebar from "./components/layout/Sidebar";
 import TermsAgreementModal, { hasAgreedToTerms } from "./components/shared/TermsAgreementModal";
+import StickyTermsFooter from "./components/shared/StickyTermsFooter";
+import PrivacyReminderBanner from "./components/shared/PrivacyReminderBanner";
 
 export default function Layout({ children, currentPageName }) {
   const [agreed, setAgreed] = useState(hasAgreedToTerms());
@@ -11,10 +13,12 @@ export default function Layout({ children, currentPageName }) {
       <div className="fixed top-0 left-0 right-0 h-1 z-50" style={{ background: "linear-gradient(90deg, #400070 0%, #6B2FB9 100%)" }} />
       <Sidebar currentPage={currentPageName} />
       <main className="lg:pl-64 pt-1">
-        <div className="p-4 pt-16 lg:pt-8 lg:p-8 max-w-7xl mx-auto">
+        <div className="p-4 pt-16 lg:pt-8 lg:p-8 pb-10 max-w-7xl mx-auto">
+          <PrivacyReminderBanner />
           {children}
         </div>
       </main>
+      <StickyTermsFooter />
       {!agreed && <TermsAgreementModal onAgree={() => setAgreed(true)} />}
     </div>
   );
